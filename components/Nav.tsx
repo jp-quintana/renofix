9b8d7fc4f55e7a8fcf07da6621f8c1eb106a3de9
jp-quintana <juanquintana1996@gmail.com>
2023-10-15 22:25:40 -0300

    WIP2: Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header>
+      <div className="nav-container flex justify-between">
+        <p>Logo</p>
+        <nav>
+          <ul className="flex gap-[10px]">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Nav;
