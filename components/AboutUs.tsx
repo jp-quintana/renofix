483327b00083960be4a6d0d5b84e9fe8234cb094
jp-quintana <juanquintana1996@gmail.com>
2023-10-24 23:58:10 -0300

    WIP6: About Us

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -2,8 +2,9 @@ const AboutUs = () => {
   return (
     <section id="aboutUs">
       <div className="main-container">
-        <h2 className="text-right">
-          Discover Renofix: Crafting Dreams into Reality
+        <h2 className="text-right text-[54px] font-bold">
+          Discover Renofix: <br />
+          Crafting Dreams into Reality
         </h2>
       </div>
     </section>
