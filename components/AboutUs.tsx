4a382a65d6b95e91b9ececc26c65858cd42634d6
jp-quintana <juanquintana1996@gmail.com>
2023-10-25 23:58:26 -0300

    WIP7: About Us

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -6,6 +6,7 @@ const AboutUs = () => {
           Discover Renofix: <br />
           Crafting Dreams into Reality
         </h2>
+        <p>Welcome to Renofix, where we specialize in crafting dreams into reality. Our passion lies in transforming your imagination into tangible, awe-inspiring spaces. With creativity at our core and craftsmanship in our hands, we breathe life into your visions. Whether it's your cherished home or valued commercial space, we are dedicated to making your dreams come true.</p>
       </div>
     </section>
   );
